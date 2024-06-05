1ec2b38701a08b346bf08aadba56c21a4ccb0a91
2y4n <163217935+2y4n@users.noreply.github.com>
2024-06-05 09:29:39 +0300

    Update erthrag.py

--- erthrag.py
+++ erthrag.py
@@ -49,7 +49,9 @@ vectorstore = DocArrayInMemorySearch.from_documents(splitted, embedding)
 
 # Set Pinecone API key from Streamlit secrets
 PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
-pinecone.init(api_key=PINECONE_API_KEY)
+os.environ['PINECONE_API_KEY'] = PINECONE_API_KEY
+pc = Pinecone(api_key=os.environ['PINECONE_API_KEY'])
+index = pc.Index("erth")
 
 index_name = "erth"
 
